fa5e30aea4f8999cfca862c5ae9981e9d9acca91
Kalyan Kumar Thanguturi <81627943+kalyan114@users.noreply.github.com>
2022-09-02 15:58:05 +0530

    Create 35.sql

---
+++ 35.sql
@@ -0,0 +1,7 @@
+SQL> select title from course where dept_name='Comp. Sci.' and credits=3;
+
+TITLE
+--------------------------------------------------
+Robotics
+Image Processing
+Database System Concepts
