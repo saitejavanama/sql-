df4d7ea46885d5bdb36dd5d27c08f2b00caf2b3e
Kalyan Kumar Thanguturi <81627943+kalyan114@users.noreply.github.com>
2022-09-02 15:47:56 +0530

    Create 32.sql

---
+++ 32.sql
@@ -0,0 +1,21 @@
+SQL> select dept_name from instructor;
+
+DEPT_NAME
+--------------------
+Comp. Sci.
+Finance
+Music
+Physics
+History
+Physics
+Comp. Sci.
+History
+Finance
+Biology
+Comp. Sci.
+
+DEPT_NAME
+--------------------
+Elec. Eng.
+
+12 rows selected.
