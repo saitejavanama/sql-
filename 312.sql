f33ec29ef649a077a04f22bc1eaa5fc0f14ee19d
Kalyan Kumar Thanguturi <81627943+kalyan114@users.noreply.github.com>
2022-09-02 16:08:41 +0530

    Create 312.sql

---
+++ 312.sql
@@ -0,0 +1,21 @@
+SQL> select * from instructor order by salary desc,name asc;
+
+ID    NAME                 DEPT_NAME                SALARY
+----- -------------------- -------------------- ----------
+22222 Einstein             Physics                   95000
+83821 Brandt               Comp. Sci.                92000
+12121 Wu                   Finance                   90000
+33456 Gold                 Physics                   87000
+98345 Kim                  Elec. Eng.                80000
+76543 Singh                Finance                   80000
+45565 Katz                 Comp. Sci.                75000
+76766 Crick                Biology                   72000
+10101 Srinivasan           Comp. Sci.                65000
+58583 Califieri            History                   62000
+32343 El Said              History                   60000
+
+ID    NAME                 DEPT_NAME                SALARY
+----- -------------------- -------------------- ----------
+15151 Mozart               Music                     40000
+
+12 rows selected.
